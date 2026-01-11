5ea868847bfbdef0b99b2ecd95731d3ddcabad87
curtasdeveloper <louisecurtandrei.pacete@gmail.com>
2026-01-11 21:26:28 +0800

    Added another two version of palindrome checker using reverse() and recursive function

--- Basic-Concept/isPalindrome.js
+++ Basic-Concept/isPalindrome.js
@@ -14,21 +14,45 @@ export function twoPointersPalindromeChecker(str) {
     return true
 }
 
-export function reversedStringPalindrome(str) {
+export function reversedStringPalindromeUsingForLoop(str) {
     str = str.split(" ").join("").toLowerCase() // for spaces and cases
     let reversedString = ""
 
-    for (let i = str.length-1; i > 0; i--) {
+    for (let i = str.length-1; i >= 0; i--) {
         reversedString += str[i]
     }
 
-    return reversedString !== str
+    return reversedString === str
+}
+
+export function reversedStringPalindromeUsingReverseMethod(str) {
+    const cleaned = str.split(" ").join("").toLowerCase()
+    return cleaned === cleaned.split("").reverse().join("")
+}
+
+export function reversedStringPalindromeUsingRecursion(str) {
+    const cleaned = str.split(" ").join("").toLowerCase()
+
+    const recursivelyReverse = (text) =>  {
+        if (text.length === 1) {
+            return text
+        }
+        return recursivelyReverse(text.substring(1)) + text[0]
+    }
+    
+    const reversed_str = recursivelyReverse(cleaned)
+    return cleaned === reversed_str
 }
 
 let palindrome_text = "alila"
 let notPalindromeText = "ulirat"
 
 console.log(twoPointersPalindromeChecker(palindrome_text), "Must be true")
-console.log(twoPointersPalindromeChecker(palindrome_text), "Must be true")
+console.log(reversedStringPalindromeUsingForLoop(palindrome_text), "Must be true")
+console.log(reversedStringPalindromeUsingReverseMethod(palindrome_text), "Must be true")
+console.log(reversedStringPalindromeUsingRecursion(palindrome_text), "Must be true")
+
 console.log(twoPointersPalindromeChecker(notPalindromeText), "Must be false")
-console.log(twoPointersPalindromeChecker(notPalindromeText), "Must be false")
+console.log(reversedStringPalindromeUsingForLoop(notPalindromeText), "Must be false")
+console.log(reversedStringPalindromeUsingReverseMethod(notPalindromeText), "Must be false")
+console.log(reversedStringPalindromeUsingRecursion(notPalindromeText), "Must be false")
